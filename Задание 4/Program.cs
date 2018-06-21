470f461ba37a12b1541f7b31bb352186cf1f47ce
Dreamcatcherss <4ydak17@gmail.com>
2018-06-21 10:58:55 +0300

    lesson 3

---
+++ Задание 4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            double R = 12.25;
+            double h = 10.12;
+            double V = 0;
+            double S = 0;
+
+            V = pi * (Math.Pow(R,2)) * h;
+
+            Console.Write("Объем цилиндра равен ");
+            Console.WriteLine(V);
+
+            S =  2 * pi * (Math.Pow(R, 2)) * (R + h);
+
+            Console.Write("Площадь цилиндра равна ");
+            Console.WriteLine(S);
+
+
+            Console.ReadKey();
+               
+        
+        }
+    }
+}
---
+++ Задание 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            byte _Identifier = 0;
+            byte \u006fIdentifier = 0;
+
+            // byte uberflu? = 0;
+            // byte &myVar = 0;
+            //Создание переменной с название &myVar и uberflu? недопустипо, потому что содержит недопустимые символы (&,?) в названии
+
+            byte myVariable = 0; 
+        }
+    }
+}
